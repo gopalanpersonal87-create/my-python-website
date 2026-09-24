bb7671eb1f36eca075ff89582a5f30f0bc63107c
Gopalan <gopalan.balu87@gmail.com>
2026-09-25 00:02:02 +0530

    youtube downloader

--- app.py
+++ app.py
@@ -6,5 +6,39 @@ app = Flask(__name__)
 def home():
     return "<h1>Hello! My Python Website is Working!</h1>"
 
+
+@app.route("/youtube-downloader", methods=["POST"])
+def youtube_downloader():
+
+    url = request.form.get("url")
+
+    if not url:
+        return jsonify({
+            "success": False,
+            "message": "YouTube URL is required"
+        }), 400
+
+    options = {
+        "format": "bestvideo+bestaudio/best",
+        "merge_output_format": "mp4",
+        "outtmpl": "%(title)s.%(ext)s"
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(options) as ydl:
+            ydl.download([url])
+
+        return jsonify({
+            "success": True,
+            "message": "Download completed"
+        })
+
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "message": str(e)
+        }), 500
+        
+        
 if __name__ == "__main__":
     app.run()
